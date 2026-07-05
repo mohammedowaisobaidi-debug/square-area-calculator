89b0776fc5a81fcbd49cb0f86c155277ff132071
mohammedowais.obaidi@hotmail.com <mohammedowais.obaidihotmail.com@Mohammeds-MacBook-Air.local>
2026-07-05 08:37:35 +0530

    Initial commit

--- circle.c
+++ circle.c
@@ -3,7 +3,7 @@ int main(){
     float radius;
     printf("Enter radius: ");
     scanf("%f", &radius);
-    printf("Area is: %f", 3.14 * radius * radius);
+    printf("Area is: %f\n", 3.14 * radius * radius);
 
     return 0;
 }
